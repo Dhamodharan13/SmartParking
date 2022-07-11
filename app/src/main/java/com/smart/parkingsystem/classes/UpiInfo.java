c1a3701f7c1e0de09b762bdf5f5d0e56fc42a866
Dhamodharan13 <dhamodharandhamu1032@gmail.com>
2022-07-11 08:24:59 +0530

    SmartParking

---
+++ app/src/main/java/com/smart/parkingsystem/classes/UpiInfo.java
@@ -0,0 +1,13 @@
+package com.smart.parkingsystem.classes;
+
+
+public class UpiInfo {
+    public String upiId,upiName;
+
+    public UpiInfo(){}
+
+    public UpiInfo(String upiId, String upiName){
+        this.upiId=upiId;
+        this.upiName=upiName;
+    }
+}
